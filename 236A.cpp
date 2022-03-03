6e873e224abad1c569feda3fe322c01438a4a50b
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-03-02 22:07:16 -0300

    Update 236A.cpp

--- 236A.cpp
+++ 236A.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-    
+    string nome;
+    cin >> nome;
+    string nomebase;
+    for (int i = 0; i < nome.length(); i++) {
+        if(nomebase.find(nome[i])) {
+            nomebase.push_back(nome[i]);
+        }
+    }
+    cout << nomebase << endl;
     return 0;
 }
