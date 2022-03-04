3aa8597ad30a7d61e9ccd37b3e070ec8f6b379c8
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-03-04 15:12:18 -0300

    declara long long a partir de agr viu :)

--- Round #774/ex1.cpp
+++ Round #774/ex1.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 using namespace std;
+typedef long long ll;
 
 int main() {
     int t;
     cin >> t;
-    int n, s;
-    for (int i = 0; i < t; i++) {
+    ll n, s;
+    while (t--) {
         cin >> n >> s;
-        if (s == 0) {
-            cout << 0 << endl;
-        }
-        else {
-            int meuValor = n*n;
-            int restoDaDivisao = s % meuValor;
-            if (restoDaDivisao == 0) {
-                cout << s / meuValor << endl;
-            }
-            else {
-                int teste = s - restoDaDivisao;
-                cout << teste / meuValor << endl;
-            }
-        }
-
+        cout << s / (n*n) << endl;
     }
     return 0;
 }
