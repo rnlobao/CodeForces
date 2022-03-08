50bbc0ed311134ff5ec347924a332f8c4186c0ce
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-03-07 21:45:51 -0300

    its harder than you think

---
+++ Round #774/ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll numberOfCases;
+    cin >> numberOfCases;
+    for (int i = 0; i < numberOfCases; i++) {
+        ll numberOfNumbers;
+        cin >> numberOfNumbers;
+        for (int j = 0; j < numberOfNumbers; j++) {
+            
+        }
+    }
+    return 0;
+}
